78e1b7e6e642690748f925f15f6a96f76e5ebfae
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-11-09 20:56:43 -0500

    computers choice working randomly

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+//Gloabl variable
+const rocks = "rock";
+const papers = "papers";
+const scissors = "scissor";
+// Get the computers choice
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber === 1) {
+        console.log(rocks);
+    } else if(randomNumber === 2) {
+        console.log(papers);
+    } else {
+        console.log(scissors);
+    }
+    console.log(randomNumber);
+}
+getComputerChoice()
