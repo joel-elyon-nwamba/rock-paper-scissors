648a404d1e55ff332bd9b69479004675d19f5adf
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-11-13 23:25:07 -0500

    game added

--- js/script.js
+++ js/script.js
@@ -1,36 +1,59 @@
-console.log("hello world");
-//Gloabl variable
+// Global variables
 const rocks = "rock";
-const papers = "papers";
-const scissors = "scissor";
-// score variables
+const papers = "paper";
+const scissors = "scissors";
+
+// Score variables
 let humanScore = 0;
 let computerScore = 0;
 
-
-// Get the computers choice
+// Get the computer's choice
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if(randomNumber === 1) {
-        console.log(rocks);
-    } else if(randomNumber === 2) {
-        console.log(papers);
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return rocks;
+    } else if (randomNumber === 1) {
+        return papers;
     } else {
-        console.log(scissors);
-    }
-    console.log(randomNumber);
-}
-getComputerChoice();
-// users choice of selection
-function getHumanChoice() {
-    let userPrompt = prompt("Select Rock, paper, or scissor");
-    if(userPrompt === "rock") {
-        console.log(rocks)
-    } else if(userPrompt === "paper") {
-        console.log(papers)
-    } else {
-        console.log(scissors)
+        return scissors;
     }
 }
 
-getHumanChoice();
+// Get the human's choice
+function getHumanChoice() {
+    let userPrompt = prompt("Select rock, paper, or scissors").toLowerCase();
+    while (![rocks, papers, scissors].includes(userPrompt)) {
+        userPrompt = prompt("Invalid choice. Please select rock, paper, or scissors").toLowerCase();
+    }
+    return userPrompt;
+}
+
+// Playing a round
+function playRound(humanChoice, computerChoice) {
+    console.log(`You chose: ${humanChoice}`);
+    console.log(`Computer chose: ${computerChoice}`);
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+        return "tie";
+    } else if (
+        (humanChoice === rocks && computerChoice === scissors) ||
+        (humanChoice === papers && computerChoice === rocks) ||
+        (humanChoice === scissors && computerChoice === papers)
+    ) {
+        console.log("You win this round!");
+        humanScore++;
+        return "human";
+    } else {
+        console.log("Computer wins this round!");
+        computerScore++;
+        return "computer";
+    }
+}
+
+// Run the game
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
+
+console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
