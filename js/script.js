8dd502070b0c8cb7a23a17e0eda46a01ea3e16a6
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-12-18 20:42:04 -0500

    need to change display to scoreboard and display on actual screen

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 // Grab the DOM for buttons, Score displaye
+let choiceOfComputer = "";
+let playerChoice = "";
 // Display Scores 
-const displayScore1 = document.getElementById("score1");
-const displayScore2 = document.getElementById("score2");
+let displayScore1 = document.getElementById("score1");
+let displayScore2 = document.getElementById("score2");
 
 const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("paper");
@@ -9,6 +11,7 @@ const scissorBtn = document.getElementById("scissors");
 console.log(rockBtn.id);
 console.log(paperBtn.id);
 console.log(scissorBtn.id);
+const buttons = document.querySelectorAll("button");
 // Reset Game
 const resetGame= document.getElementById("reset-game");
 
@@ -16,34 +19,59 @@ const resetGame= document.getElementById("reset-game");
 let computerScore = 0;
 let playerScore = 0;
 
-// Function for the players choice
+
+
+// Function for the players choice only be triggered with an event handler
 const playersChoice = (e) => {
     if(e.target.id === rockBtn) {
-        return "rock";
+        console.log("click me!")
+        return playerChoice = "rock";
     } else if(e.target.id === paperBtn.id) {
-        return "paper"
+        return playerChoice = "paper"
     } else if(e.target.id === scissorBtn.id) {
-        return "scissor"
+        return playerChoice = "scissor"
     } else {
         return "This is an error please enter the correct info for the game to work"
     }
 }
-
-console.log(playersChoice("rock"));
+rockBtn.addEventListener("click", playersChoice);
 
 // Computer's random spot 
 const computerChoice = () => {
 let randomNumber = Math.floor(Math.random() * 3) + 1;
 console.log(randomNumber)
 if(randomNumber === 1) {
-return "rock";
+return choiceOfComputer = "rock";
 } else if(randomNumber === 2) {
-return "paper";
+return choiceOfComputer = "paper";
 } else if(randomNumber === 3) {
-return "scissors";
+return choiceOfComputer = "scissors";
 } else {
 return "Wrong task"
 }
 }
 
-console.log(computerChoice())
+console.log(computerChoice());
+
+// Get the results of the game accordingly
+const  gameResults = (e) => {
+   const computerPlay = computerChoice();
+   const playerSelect = playersChoice(e);
+   if(playerSelect === "rock" && computerPlay === "scissors") {
+   displayScore1++
+   } else if(playerSelect === "scissors" && computerPlay === "rock"){
+   displayScore2++
+   } else if(playerSelect === "paper" && computerPlay === "rock") {
+   displayScore1++
+   } else if(playerSelect === "paper" && computerPlay === "scissor"){
+   displayScore2++
+   } else if(playerSelect === "scissor" && computerPlay === "paper"){
+   displayScore1++
+   } else if(playerSelect === computerPlay){
+   return "it's a tie";
+   }
+}
+
+buttons.forEach(buttons => {
+    buttons.addEventListener("click", gameResults);
+})
